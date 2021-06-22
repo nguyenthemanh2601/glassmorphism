2cc027e33c12afac4cced8c54d7b9e7822eb4d49
Nguyễn Thế Mạnh <hoangtupro.a4@gmail.com>
2021-06-22 16:04:39 +0700

    init

---
+++ assets/js/app.js
@@ -0,0 +1,63 @@
+$(function () {
+  $(".menu-link").click(function () {
+    $(".menu-link").removeClass("is-active");
+    $(this).addClass("is-active");
+  });
+  $(".main-header-link").click(function () {
+    $(".main-header-link").removeClass("is-active");
+    $(this).addClass("is-active");
+  });
+  $(".dropdown").on("click", function (e) {
+    $(".content-wrapper").addClass("overlay");
+    e.stopPropagation();
+  });
+  $(document).on("click", function (e) {
+    if ($(e.target).is(".dropdown") === false) {
+      $(".content-wrapper").removeClass("overlay");
+    }
+  });
+  $(".status-button:not(.open)").on("click", function (e) {
+    $(".overlay-app").addClass("is-active");
+  });
+  $(".pop-up .close").click(function () {
+    $(".overlay-app").removeClass("is-active");
+  });
+});
+
+const dropdowns = document.querySelectorAll(".dropdown");
+dropdowns.forEach((dropdown) => {
+  dropdown.addEventListener("click", (e) => {
+    e.stopPropagation();
+    dropdowns.forEach((c) => c.classList.remove("is-active"));
+    dropdown.classList.add("is-active");
+  });
+});
+
+$(".search-bar input")
+.focus(function () {
+  $(".header").addClass("wide");
+})
+.blur(function () {
+  $(".header").removeClass("wide");
+});
+
+$(document).click(function (e) {
+  var container = $(".status-button");
+  if (!container.is(e.target) && container.has(e.target).length === 0) {
+    $(".dropdown").removeClass("is-active");
+  }
+});
+
+$(".status-button:not(.open)").click(function () {
+  $(".pop-up").addClass("visible");
+});
+
+$(".pop-up .close").click(function () {
+  $(".pop-up").removeClass("visible");
+});
+
+const toggleButton = document.querySelector('.dark-light');
+
+toggleButton.addEventListener('click', () => {
+  document.body.classList.toggle('light-mode');
+});
